b5f1a8502bbb2179bd51c611015d81ecd5662932
gusleaooliveira <gus.leaono@gmail.com>
2023-10-27 00:08:09 -0300

    structure task created

--- src/tasks/task.module.ts
+++ src/tasks/task.module.ts
@@ -5,7 +5,7 @@ import * as TaskController from './task.controller'
 const router = Router()
 
 router.get('/tasks', TaskController.getAllTasks)
-router.get('/tasks', TaskController.createTask)
-router.post('/tasks/:id', TaskController.deleteTask)
+router.post('/tasks', TaskController.createTask)
+router.delete('/tasks/:id', TaskController.deleteTask)
 
 export default router
